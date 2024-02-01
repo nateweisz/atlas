63af7be639d060bdf13d3d15aec1268551ff6c39
Nathan Weisz <25nathanwe@piedmont.local>
2024-02-01 08:59:05 -0800

    feat(protocol): commit hash on `RequestDeploymentPacket`

--- protocol/src/main/java/me/nateweisz/protocol/clientbound/S2CRequestDeploymentPacket.java
+++ protocol/src/main/java/me/nateweisz/protocol/clientbound/S2CRequestDeploymentPacket.java
@@ -10,17 +10,20 @@ public class S2CRequestDeploymentPacket implements Packet {
     private final UUID deploymentId; // used for the callback when it is deployed.
     private final String codeProvider; // prob only `git` for now
     private final String path; // the path (ex. nateweisz/atlas) likely only git support for now.
+    private final String commitHash; // TODO: in the future this should likely be a metadata tag when we support non git stuff.
     
-    public S2CRequestDeploymentPacket(UUID deploymentId, String codeProvider, String path) {
+    public S2CRequestDeploymentPacket(UUID deploymentId, String codeProvider, String path, String commitHash) {
         this.deploymentId = deploymentId;
         this.codeProvider = codeProvider;
         this.path = path;
+        this.commitHash = commitHash;
     }
     
     public S2CRequestDeploymentPacket(Buffer buffer) {
         this.deploymentId = UUID.fromString(buffer.getString(1, buffer.length()));
         this.codeProvider = buffer.getString(2, buffer.length());
         this.path = buffer.getString(3, buffer.length());
+        this.commitHash = buffer.getString(4, buffer.length());
     }
     
     @Override 
@@ -28,5 +31,6 @@ public class S2CRequestDeploymentPacket implements Packet {
         buffer.appendString(deploymentId.toString());
         buffer.appendString(codeProvider);
         buffer.appendString(path);
+        buffer.appendString(commitHash);
     }
 }
