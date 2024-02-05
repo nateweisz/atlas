d89170b497bf7635b4b7c4e402314c864ebe1ddf
Nathan Weisz <151496498+nateweisz@users.noreply.github.com>
2024-02-04 20:28:07 -0800

    Create WrappedBuffer.java

---
+++ protocol/src/main/java/WrappedBuffer.java
@@ -0,0 +1,69 @@
+import io.vertx.core.buffer.Buffer;
+
+import java.nio.charset.StandardCharsets;
+
+public class WrappedBuffer {
+    private final Buffer buffer;
+    private int currentIndex;
+    
+    // byte = 1
+    // int = 4
+    // long = 8
+    // double = 8
+    // float = 4
+    // short = 2
+    
+    public WrappedBuffer(Buffer buffer) {
+        this.buffer = buffer;
+        this.currentIndex = 0;
+    }
+    
+    public WrappedBuffer(Buffer buffer, int initialIndex) {
+        this.buffer = buffer;
+        this.currentIndex = initialIndex;
+    } 
+    
+    public byte nextByte() {
+        byte value = buffer.getByte(currentIndex);
+        currentIndex += 1; // easier to understand what we are increasing it by doing this.
+        return value;
+    }
+    
+    public int nextInt() {
+        int value = buffer.getInt(currentIndex);
+        currentIndex += 4;
+        return value;
+    }
+    
+    public long nextLong() {
+        long value = buffer.getLong(currentIndex);
+        currentIndex += 8;
+        return value;
+    }
+    
+    public double nextDouble() {
+        double value = buffer.getDouble(currentIndex);
+        currentIndex += 8;
+        return value;
+    }
+    
+    public float nextFloat() {
+        float value = buffer.getFloat(currentIndex);
+        currentIndex += 4;
+        return value;
+    }
+    
+    public short nextShort() {
+        short value = buffer.getShort(currentIndex);
+        currentIndex += 2;
+        return value;
+    }
+    
+    public String nextString() {
+        // first we will get an int before it that will represent the length of the string
+        int stringLength = nextInt();
+        String value = buffer.getString(currentIndex, currentIndex + stringLength);
+        currentIndex += value.getBytes(StandardCharsets.UTF_8).length;
+        return value;
+    }
+}
